8f76edc9758ab1c5d4902e31321eebe47fb106c8
seongjinbrian <seongjin2788@gmail.com>
2021-07-26 21:15:19 +0900

    first commit

---
+++ back/src/main/java/seongjin/appetit/AppetitApplication.java
@@ -0,0 +1,13 @@
+package seongjin.appetit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppetitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppetitApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/seongjin/appetit/AppetitApplicationTests.java
@@ -0,0 +1,13 @@
+package seongjin.appetit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppetitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
